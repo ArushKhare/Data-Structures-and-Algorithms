00745c930aab58a8ce431c016d30c15a33b644bd
Arush Khare <56801812+ArushKhare@users.noreply.github.com>
2024-02-15 23:58:11 -0800

    Update QuadraticFunction.py

--- QuadraticFunction.py
+++ QuadraticFunction.py
@@ -1,13 +1,11 @@
 import math
 
-
 class Quadratic(object):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
         self.discriminant = pow(b, 2) - (4 * a * c)
-
 
     def roots(self):
         sqrt = None
@@ -18,8 +16,9 @@ class Quadratic(object):
             return -self.b / (2 * self.a)
 
         if self.discriminant > 0:
-            if math.sqrt(self.discriminant).is_integer():
-                sqrt = math.sqrt(self.discriminant)
+            temp = math.sqrt(self.discriminant)
+            if temp == int(temp):
+                sqrt = temp
                 sqrt /= 2 * self.a
                 res1, res2 = None, None
                 res1 = sqrt + (-self.b) / (2 * self.a)
@@ -32,7 +31,6 @@ class Quadratic(object):
                 res2 = f"(-{str(self.b)} - {sqrt}) / {2 * self.a}"
                 return [res1, res2]
 
-
     def num_of_solutions(self):
         if self.discriminant < 0:
             return 0
@@ -41,17 +39,19 @@ class Quadratic(object):
         if self.discriminant > 0:
             return 2
 
-
     def y_intercept(self):
         return self.c
 
-
     def function(self, x):
         return (self.a * pow(x, 2)) + (self.b * x) + self.c
+    
+    def format(self):
+        return f'f(x) = ({self.a})x^2 + ({self.b})x + {self.c}'
 
 
-q = Quadratic(1, 8, 8)
+q = Quadratic(1, 6, 4)
 
-print(q.roots())
-
-
+print("roots:", q.roots())
+print("function:", q.format())
+print("y intercept:", q.y_intercept())
+print('f(0):', q.function(0))
